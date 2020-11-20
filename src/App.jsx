82df827d31df8180355bd5453f37c07c1629f936
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-11-20 15:30:06 -0800

    fix: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
-
+import {Router, Route, Switch, Redirect} from 'react-router-dom';
+import { createHashHistory } from 'history';
 import Home from './pages/Home';
 
+
+const history = createHashHistory({
+  basename: '',
+  hashType: 'noslash',
+  getUserConfirmation: (message, callback) => callback(window.confirm(message)),
+});
 function App() {
   return (
-    <div className="App">
-        <Home />
-    </div>
+    <Router history={history}> 
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Navbar.css";
+import {Link} from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="Navbar">
       <ul class="nav">
-        <li class="logo">Portfolio</li>
-        <li tabindex="0">
+
+        <li class="logo">
+            <Link to="/" >Portfolio</Link></li>
+
+        <div class="name">Mary Delos Reyes</div>
+        {/* <li tabindex="0">
           <i data-feather="home">About Me</i>
         </li>
         <li tabindex="0">
@@ -14,7 +19,7 @@ function Navbar() {
         </li>
         <li tabindex="0">
           <i data-feather="list">Work</i>
-        </li>
+        </li> */}
         <li tabindex="0">
           <i data-feather="list">Contact</i>
         </li>
