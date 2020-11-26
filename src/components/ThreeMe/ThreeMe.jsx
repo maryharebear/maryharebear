e15720e5320f540dbc10346d270d2be46d404155
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-11-26 03:04:23 -0800

    add: three boxes

--- src/components/ThreeMe/ThreeMe.jsx
+++ src/components/ThreeMe/ThreeMe.jsx
@@ -2,13 +2,45 @@ import React from "react";
 import "./ThreeMe.css";
 import { Link } from "react-router-dom";
 
+import AboutMe from "../../assests/about-me.jpg";
+import Designs from "../../assests/designs.jpg";
+import Work from "../../assests/work.jpg";
+
 // import Logo from "../../assests/logo.jpg";
 // import Signature from "../../assests/signature.jpg";
 
 function ThreeMe() {
   return (
-    <main className="ThreeBoxes">
-     
+    <main className="ThreeBoxes wrapper">
+      <section class="slider">
+        <div class="images">
+          <div class="image">
+            {" "}
+            <img
+              src={AboutMe}
+              
+            />
+          </div>{" "}
+        </div>
+        <div class="images">
+          <div class="image">
+            {" "}
+            <img
+              src={Designs}
+              
+            />
+          </div>{" "}
+        </div>
+        <div class="images">
+          <div class="image">
+            {" "}
+            <img
+              src={Work}
+              
+            />
+          </div>{" "}
+        </div>
+      </section>
     </main>
   );
 }
