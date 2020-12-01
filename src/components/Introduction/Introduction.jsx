91be12d6be630ef69d0a078acadabf12572d66d1
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-11-30 18:16:02 -0800

    add: about me page

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -5,12 +5,24 @@ import ART from "../../assests/test-3d.png";
 
 function Introduction() {
   return (
-    <div className="Introduction">
-      <h1>🌵 Hi I'm Mary (she/her), a second-year Computer Science and Engineering student. I am also an aspring Web Designer passionate about UI/UX design. </h1>
+    <section className="Landing">
+      <div className="Introduction">
+        <h1>
+          🌵 Hi! I'm Mary (she/her), a second-year Computer Science and
+          Engineering student. I am also an aspring Web Designer passionate
+          about UI/UX design.{" "}
+        </h1>
         <div className="Art">
-        <img src={ART} alt="3D ART" />
+          <img src={ART} alt="3D ART" />
         </div>
-    </div>
+      </div>
+      <a href="#" className="scroll-down">
+        <div class="scroll">
+          <span class="scroll-dot"></span>
+        </div>
+        <div class="arrow-down"></div>
+      </a>
+    </section>
   );
 }
 
--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -3,13 +3,36 @@ import "./AboutMe.css";
 
 import Navbar from "../../components/Navbar/Navbar";
 
+import Selfie from "../../assests/selfie-me.png";
 
 function AboutMe() {
   return (
       <>
       <Navbar />
     <main className="AboutMe">
-        
+    <section className="Mary">
+    <div className="Selfie">
+          <img src={Selfie} alt="3D ART" />
+        </div>
+        <h1>
+          👋🏼 Nice to meet you! <br></br> <br></br>
+          My name is Mary Francine Delos Reyes. I was born in the Philippines 
+          and half-raised in California. I am currently majoring in Computer Science
+          and Engineering at University of California, Merced. I am pursuing 
+          UI/UX design and currently looking for internships to practice and learn
+          more skills in design-related work with tech.
+          <br></br> <br></br>
+          I am also currently a frontend lead for a hackathon organization at UC Merced,
+          called HackMerced. Also, a Design SIG lead for ACM (Association for Computing Machinery)
+          where I share resources, host workshops, and help students to explore their creative
+          side. I love attending designathons and hosted one with my HackMerced organization named 
+          DesignMerced.
+          <br></br> <br></br>
+          Besides design, I have passion for dancing, gaming, traveling, and creating stuff.
+          So let's collaborate and talk more about design and Harry Styles! ♡
+        </h1>
+       
+      </section>
       
     </main>
     </>
